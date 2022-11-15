eeb46b451ff918af673cb449e370b12e2aa61a53
singh36Pushpendra <psthakur.1999@gmail.com>
2022-11-15 12:06:46 +0530

    Added: UC1 Calculated total fare for journey

---
+++ src/cab/ig/CabInvoiceGenerator.java
@@ -0,0 +1,22 @@
+package cab.ig;
+
+public class CabInvoiceGenerator {
+
+    private double time, distance;
+    private static double RS_PER_MIN = 1, RS_PER_KM = 10, RS_MIN_FARE = 5;
+
+    public CabInvoiceGenerator(double time, double distance) {
+        this.time = time;
+        this.distance = distance;
+    }
+
+    public double calculateFare() {
+        double totalFare = distance * RS_PER_KM + time * RS_PER_MIN;
+
+        if (totalFare < RS_MIN_FARE)
+            totalFare = RS_MIN_FARE;
+
+        return totalFare;
+    }
+
+}
--- src/cab/ig/CabServiceMain.java
+++ src/cab/ig/CabServiceMain.java
@@ -3,5 +3,8 @@ package cab.ig;
 public class CabServiceMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Cab Service!");
+
+        CabInvoiceGenerator invoiceGenerator = new CabInvoiceGenerator(15, 20);
+        System.out.println("Total fare of journey = " + invoiceGenerator.calculateFare() + " Rs.");
     }
 }
